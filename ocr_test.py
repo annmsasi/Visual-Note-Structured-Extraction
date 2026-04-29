2b4f3869a1be0e347d83f987a20bfbf4db90d5d7
Ann <asasi@ucsc.edu>
2026-04-29 14:37:58 -0700

    added tesseract

---
+++ ocr_test.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from PIL import Image
+import pytesseract
+
+image_path = next(Path("data/inbox").glob("*"))
+
+text = pytesseract.image_to_string(Image.open(image_path))
+
+print("IMAGE:", image_path.name)
+print("OCR TEXT:")
+print(text)
