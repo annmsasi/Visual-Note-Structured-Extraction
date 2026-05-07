81fe66d6cb6ee80c1b9ca1c7af54e2c4ea493279
Ann <asasi@ucsc.edu>
2026-05-07 15:36:56 -0700

    added llm

--- extract_test.py
+++ extract_test.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 import base64
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI()
 
